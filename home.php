9973787ea73ac2f63c41a0f9406c28373d4fbc8f
German Cabarcas <gcc@unironical.com>
2024-10-06 11:40:57 -0500

    Blog list template

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <div class="container pt-10">
+        <?php if ( have_posts() ) : ?>
+
+            <header class="page-header">
+                <h1>Blog</h1>
+            </header>
+
+            <div class="row row-cols-md-2 row-cols-lg-3 gy-5 gy-lg-6 gx-3">
+                <?php
+                while ( have_posts() ) :
+                    the_post();
+                    get_template_part( 'template-parts/summary');
+                endwhile;
+                ?>
+            </div>
+        <?php
+        avasol_pagination_links();
+
+        else :
+
+            get_template_part( 'template-parts/content', 'none' );
+
+        endif;
+        ?>
+    </div>
+</div>
+<?php get_footer(); ?>
---
+++ template-parts/summary.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package avasol
+ */
+
+?>
+
+<?php if (is_singular()) {
+return ;
+} ?>
+
+<article id="post-<?php the_ID(); ?>" class="article-summary">
+    <header class="entry-header">
+    <?php
+        if (has_post_thumbnail()):
+        $img_id = get_post_thumbnail_id();?>
+        <div class="summary-image">
+            <?php echo avasol_lazy_image($img_id, 'medium_large', 'img-fluid') ?>
+        </div>
+    <?php else : ?>
+        <div class="summary-image summary-image-no-featured">
+                <img src="<?= esc_url( wp_get_attachment_url(1385) ); ?>" width="94" height="75" class="custom-logo" alt="<?php bloginfo( 'name' ); ?>">
+        </div>
+    <?php endif; ?>
+
+        <?php the_title('<h2 class="entry-title summary-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
+    </header>
+
+    <div class="container pt-0">
+        <div class="entry-meta summary-meta">
+        <?php
+            avasol_posted_on();
+            avasol_posted_by();
+            ?>
+        </div>
+
+        <div class="entry-content summary-content">
+            <?php
+        $matches = [];
+        $has_match = preg_match(
+            '/<!--\s+(?P<closer>\/)?wp:(?P<namespace>[a-z][a-z0-9_-]*\/)?(?P<name>[a-z][a-z0-9_-]*)\s+(?P<attrs>{(?:(?:[^}]+|}+(?=})|(?!}\s+\/?-->).)*+)?}\s+)?(?P<void>\/)?-->/s',
+            get_the_content(),
+            $matches,
+        );
+        if ($has_match) {
+            $block_data = json_decode($matches['attrs'], true);
+            if ($block_data && isset($block_data['data'])) {
+                $headline = $block_data['data']['textblock_headline'] ?? '';
+                $text = $block_data['data']['textblock_field'] ?? '';
+                echo '<p>' . wp_trim_words(wp_strip_all_tags(strlen($headline) > strlen($text) ? $headline : $text), 20) . '</p>';
+            } 
+        } 
+        echo ' <a href="' . get_permalink() . '">Read More</a>';
+
+            ?>
+        </div>
+    </div>
+
+</article>
